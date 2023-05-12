e2331875e913a55457d5547c6b6c9bf970a7dbf3
Borja Martí <borjamarti@outlook.com>
2023-05-12 17:16:52 +0200

    added comments explaining routes and server files

--- routes/tasks.js
+++ routes/tasks.js
@@ -2,14 +2,24 @@ const express = require('express')
 const router = express.Router()
 const tasksController = require('../controllers/tasks') 
 
+// @desc Fetch all tasks
+// @route GET /
 router.get('/', tasksController.getTasks)
 
+// @desc Fetch single task
+// @route GET /fetchTask/:id
 router.get('/fetchTask/:id', tasksController.fetchTask)
 
+// @desc Add new task
+// @route POST /addTask
 router.post('/addTask', tasksController.addTask)
 
+// @desc Toggle reminder in task
+// @route PUT /toggleReminder/:id
 router.put('/toggleReminder/:id', tasksController.toggleReminder)
 
+// @desc Delete a task
+// @route DELETE /deleteTask/:id
 router.delete('/deleteTask/:id', tasksController.deleteTask)
 
 module.exports = router
--- server.js
+++ server.js
@@ -6,17 +6,28 @@ const connectDB = require('./config/database')
 const tasksRoutes = require('./routes/tasks')
 const cors = require('cors')
 
+// Setup environment variables
 require('dotenv').config({path: './config/.env'})
 
+// Connect to the DB
 connectDB()
 
+// Allow CORS operations for dev purposes
 app.use(cors())
+
+// Middleware for parsing form data
 app.use(express.urlencoded({ extended: true }))
+
+// Middleware for parsing JSON data
 app.use(express.json())
+
+// Request logger
 app.use(logger('dev'))
 
+// Routes
 app.use('/tasks', tasksRoutes)
- 
+
+
 app.listen(process.env.PORT, ()=>{
     console.log('Server is running, you better catch it!')
 })    
